d1b63f5ea07952febbe74c6f170a45e1b222913a
Michael Ash <mikeash@MikeAir.local>
2012-12-04 20:17:46 -0500

    Load up the NSObject interface

--- MAObject.h
+++ MAObject.h
@@ -1,4 +1,69 @@
 
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+
 @interface MAObject
+
+- (BOOL)isEqual:(id)object;
+- (NSUInteger)hash;
+
+- (Class)superclass;
+- (Class)class;
+- (id)self;
+
+- (id)performSelector:(SEL)aSelector;
+- (id)performSelector:(SEL)aSelector withObject:(id)object;
+- (id)performSelector:(SEL)aSelector withObject:(id)object1 withObject:(id)object2;
+
+- (BOOL)isProxy;
+
+- (BOOL)isKindOfClass:(Class)aClass;
+- (BOOL)isMemberOfClass:(Class)aClass;
+- (BOOL)conformsToProtocol:(Protocol *)aProtocol;
+
+- (BOOL)respondsToSelector:(SEL)aSelector;
+
+- (id)retain;
+- (oneway void)release;
+- (id)autorelease;
+- (NSUInteger)retainCount;
+
+- (NSString *)description;
+
++ (void)load;
+
++ (void)initialize;
+- (id)init;
+
++ (id)new;
++ (id)alloc;
+- (void)dealloc;
+
+- (void)finalize;
+
+- (id)copy;
+- (id)mutableCopy;
+
++ (Class)superclass;
++ (Class)class;
++ (BOOL)instancesRespondToSelector:(SEL)aSelector;
++ (BOOL)conformsToProtocol:(Protocol *)protocol;
+- (IMP)methodForSelector:(SEL)aSelector;
++ (IMP)instanceMethodForSelector:(SEL)aSelector;
+- (void)doesNotRecognizeSelector:(SEL)aSelector;
+
+- (id)forwardingTargetForSelector:(SEL)aSelector;
+- (void)forwardInvocation:(NSInvocation *)anInvocation;
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector;
+
++ (NSMethodSignature *)instanceMethodSignatureForSelector:(SEL)aSelector;
+
++ (NSString *)description;
+
++ (BOOL)isSubclassOfClass:(Class)aClass;
+
++ (BOOL)resolveClassMethod:(SEL)sel;
++ (BOOL)resolveInstanceMethod:(SEL)sel;
 
 @end
